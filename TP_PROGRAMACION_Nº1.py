751754b7c81b3a5dbbd087dcfa280d4e0695c435
Braian Bascuñan <braianbascu@gmail.com>
2026-08-22 18:26:16 -0300

    Ejercicios 3 y 4

--- TP_PROGRAMACION_Nº1.py
+++ TP_PROGRAMACION_Nº1.py
@@ -1,20 +1,20 @@
-#TRABAJO PRACTICO DE PROGRAMACION Nº1 - BASCUÑAN BRAIAN 
+# TRABAJO PRACTICO DE PROGRAMACION Nº1 - BASCUÑAN BRAIAN
 
-#EJERCICIO Nº1
+# EJERCICIO Nº1
 
-#Variables utilizadas
-#nombre_cliente = ""
-#cantidad_productos= 0
-#productos = []
-#valor_producto= 0
-#descuento= ""
-#valor_total = 0
-#valor_final = 0
-#valor_ahorro = 0
-#promedio_productos = 0
+# Variables utilizadas
+# nombre_cliente = ""
+# cantidad_productos= 0
+# productos = []
+# valor_producto= 0
+# descuento= ""
+# valor_total = 0
+# valor_final = 0
+# valor_ahorro = 0
+# promedio_productos = 0
 
-#Solicitud de nombre
-#while True: 
+# Solicitud de nombre
+# while True:
 #    nombre_cliente = input("Ingrese nombre del cliente: ")
 #    if nombre_cliente =="":
 #        print("Debe ingresar un nombre para continuar")
@@ -24,8 +24,8 @@
 #    else:
 #        print("Debe ingresar solo letras")
 
-#Solicitud cantidad final de productos
-#while True: 
+# Solicitud cantidad final de productos
+# while True:
 #    cantidad_productos = input("Ingrese la cantidad de productos a comprar: ")
 
 #    if cantidad_productos == "":
@@ -38,9 +38,9 @@
 #        cantidad_productos = int(cantidad_productos)
 #        break
 
-#Carga de valores
-#for i in range(cantidad_productos):
-   
+# Carga de valores
+# for i in range(cantidad_productos):
+
 #    while True:
 #        valor_producto = input(f"ingrese el valor entero del producto {i+1}: ")
 
@@ -64,40 +64,39 @@
 
 #    valor_total += valor_producto
 
-#Diferenciacion por descuento
+# Diferenciacion por descuento
 #    if descuento.lower() == "s":
 #         valor_ahorro += valor_producto *0.10
 
-#Carga a la base de deatos
+# Carga a la base de deatos
 #    productos.append(f"Producto {i + 1} - Precio: {valor_producto} Descuento (S/N): {descuento}")
 
-#Calculos
-#valor_final = valor_total - valor_ahorro
-#promedio_productos = valor_final / cantidad_productos
+# Calculos
+# valor_final = valor_total - valor_ahorro
+# promedio_productos = valor_final / cantidad_productos
 
-#Consola
-#print(f"Cliente: {nombre_cliente}")
-#print(f"Cantidad de productos: {cantidad_productos}")
+# Consola
+# print(f"Cliente: {nombre_cliente}")
+# print(f"Cantidad de productos: {cantidad_productos}")
 
-#for producto in productos:
+# for producto in productos:
 #    print(producto)
 
-#print(f"Total sin descuentos: ${valor_total}")
-#print(f"Total con descuentos: ${valor_final:.2f}")
-#print(f"Ahorro: ${valor_ahorro:.2f}")
-#print(f"Promedio por producto: ${promedio_productos:.2f}")
-
+# print(f"Total sin descuentos: ${valor_total}")
+# print(f"Total con descuentos: ${valor_final:.2f}")
+# print(f"Ahorro: ${valor_ahorro:.2f}")
+# print(f"Promedio por producto: ${promedio_productos:.2f}")
 
 
 # EJERCICIO Nº2
 
-#Variables utilizadas
+# Variables utilizadas
 
-#usuario_correcto = "alumno"
-#contraseña_correcta = "python123"
-#acceso= False
+# usuario_correcto = "alumno"
+# contraseña_correcta = "python123"
+# acceso= False
 
-#for intento in range (1,4) :
+# for intento in range (1,4) :
 #    print(f"intento: {intento}")
 #    usuario = input("Ingrese su nombre de usuario: ")
 #    contraseña = input("Ingrese su contraseña: ")
@@ -107,12 +106,12 @@
 #        print("")
 #        acceso = True
 #        break
-#    else: 
+#    else:
 #        print("Credenciales invalidas")
 
-#if acceso == False :
+# if acceso == False :
 #    print ("Cuenta bloqueada")
-#else:
+# else:
 #    while True:
 #        print("-----MENU-----")
 #        print("")
@@ -154,80 +153,284 @@
 #                print("")
 
 
-#EJERCICIO Nº3
+# EJERCICIO Nº3
 
-#Variables utilizadas
+# # Variables utilizadas
+# operador = ""
+# lunes1 = ""
+# lunes2 = ""
+# lunes3 = ""
+# lunes4 = ""
+# martes1 = ""
+# martes2 = ""
+# martes3 = ""
 
-operador = ""
-lunes1 = ""
-lunes2 = ""
-lunes3 = ""
-lunes4 = ""
-martes1 = ""
-martes2 = ""
-martes3 = ""
+# print("---SISTEMA CENTRAL CLINICA---")
+# print("")
 
-print("---SISTEMA CENTRAL CLINICA---")
-print("")
+# while True:
+#     operador = input("ingrese el nombre del operador: ")
 
-while True:
-    operador = input("ingrese el nombre del operador: ")
+#     if operador == "":
+#         print("Debe ingresar un nombre para continuar")
+#     elif operador.isalpha():
+#         print("")
+#         break
+#     else:
+#         print("El nombre debe contener solo letras")
 
-    if operador == "":
-        print ("Debe ingresar un nombre para continuar")
-    elif operador.isalpha():
-        print("")
-        break
-    else:
-        print("El nombre debe contener solo letras")
+# print(f"Bienvenido {operador}")
+# # menu de seleccion
+# while True:
+#     print("-----MENU-----")
+#     print("")
+#     print("1- RESERVAR TURNO")
+#     print("2- CANCELAR TURNO")
+#     print("3- VER AGENDA DEL DIA")
+#     print("4- VER RESUMEN GENERAL")
+#     print("5- SALIR")
+#     print("")
 
-print(f"Bienvenido {operador}")
+#     opcion = input("Ingrese una opcion: ")
+#     # Control input ingresado
+#     if opcion == "":
+#         print("Debe ingresar una opcion para continuar")
+#         print("")
+#     elif not opcion.isdigit():
+#         print("Ingrese un valor numerico")
+#         print("")
+#     else:
+#         opcion = int(opcion)
+#         if opcion < 1 or opcion > 5:
+#             print("Error opcion fuera de rango")
+#         # Flujo de menu
 
-while True:
-    print("-----MENU-----")
-    print("")
-    print("1- RESERVAR TURNO")
-    print("2- CANCELAR TURNO")
-    print("3- VER AGENDA DEL DIA")
-    print("4- VER RESUMEN GENERAL")
-    print("5- SALIR")
-    print("")
+#         # OPCION 1
+#         elif opcion == 1:
+#             while True:
+#                 print("1- LUNES")
+#                 print("2- MARTES")
+#                 dia = input("Selecione un dia:")
 
-    opcion = input("Ingrese una opcion: ")
+#                 if dia == "":
+#                     print("Debe ingresar una opcion para continuar")
+#                     print("")
+#                 elif not dia.isdigit():
+#                     print("Ingrese un valor numerico")
+#                     print("")
+#                 else:
+#                     dia = int(dia)
 
-    if opcion == "":
-        print ("Debe ingresar una opcion para continuar")
-        print("")
-    elif not opcion.isdigit():
-        print("Ingrese un valor numerico")
-        print("")
-    else:
-        opcion = int(opcion)
+#                     if dia < 1 or dia > 2:
+#                         print("Error opcion fuera de rango")
+#                     else:
+#                         break
 
-        if opcion < 1 or opcion > 5:
-            print("Error opcion fuera de rango")
-        elif opcion == 1:
-            while True:
-                print("1- LUNES")
-                print("2- MARTES")
-                dia = input("Selecione un dia:")
+#             while True:
+#                 paciente = input("Por favor ingrese el nombre del paciente: ")
 
-                if dia == "":
-                    print ("Debe ingresar una opcion para continuar")
-                    print("")
-                elif not dia.isdigit():
-                    print("Ingrese un valor numerico")
-                    print("")
-                else:
-                    dia = int(dia)
-                
-                    if dia < 1 or dia > 2:
-                        print("Error opcion fuera de rango")
-        elif opcion == 2:
-            print("opcion 2")
-        elif opcion == 3:
-            print("opcion 3")
-        elif opcion == 4:
-            print("opcion 4")
-        elif opcion ==5:
-            break
+#                 if paciente == "":
+#                     print("Debe ingresar un nombre para continuar")
+#                 elif paciente.isalpha():
+#                     print("")
+#                     break
+#                 else:
+#                     print("Debe ingresar solo letras")
+#             # Verificacion y asignacion de turno
+#             if dia == 1:
+#                 if (
+#                     paciente == lunes1
+#                     or paciente == lunes2
+#                     or paciente == lunes3
+#                     or paciente == lunes4
+#                 ):
+#                     print("El paciente ya tiene u turno asignado")
+#                 elif lunes1 == "":
+#                     lunes1 = paciente
+#                     print("paciente asignado al turno 1")
+#                 elif lunes2 == "":
+#                     lunes2 = paciente
+#                     print("paciente asignado al turno 2")
+#                 elif lunes3 == "":
+#                     lunes3 = paciente
+#                     print("paciente asignado al turno 3")
+#                 elif lunes4 == "":
+#                     lunes4 = paciente
+#                     print("paciente asignado al turno 4")
+#                 else:
+#                     print("No hay turnos disponibles este dia")
+#             else:
+#                 if paciente == martes1 or paciente == martes2 or paciente == martes3:
+#                     print("El paciente ya tiene u turno asignado")
+#                 elif martes1 == "":
+#                     martes1 = paciente
+#                     print("paciente asignado al turno 1")
+#                 elif martes2 == "":
+#                     martes2 = paciente
+#                     print("paciente asignado al turno 2")
+#                 elif martes3 == "":
+#                     martes3 = paciente
+#                     print("paciente asignado al turno 3")
+#                 else:
+#                     print("No hay turnos disponibles este dia")
+#         # OPCION 2
+#         elif opcion == 2:
+#             while True:
+#                 print("1- LUNES")
+#                 print("2- MARTES")
+#                 dia = input("Selecione un dia:")
+
+#                 if dia == "":
+#                     print("Debe ingresar una opcion para continuar")
+#                     print("")
+#                 elif not dia.isdigit():
+#                     print("Ingrese un valor numerico")
+#                     print("")
+#                 else:
+#                     dia = int(dia)
+
+#                     if dia < 1 or dia > 2:
+#                         print("Error opcion fuera de rango")
+#                     else:
+#                         break
+#             # Verificacion y cancelacion de turno
+#             while True:
+#                 paciente = input("Por favor ingrese el nombre del paciente: ")
+
+#                 if paciente == "":
+#                     print("Debe ingresar un nombre para continuar")
+#                 elif paciente.isalpha():
+#                     print("")
+#                     break
+#                 else:
+#                     print("Debe ingresar solo letras")
+
+#             if dia == 1:
+#                 if lunes1 == paciente:
+#                     lunes1 = ""
+#                     print("Turno cancelado con exito")
+#                 elif lunes2 == paciente:
+#                     lunes2 = ""
+#                     print("Turno cancelado con exito")
+#                 elif lunes3 == paciente:
+#                     lunes3 = ""
+#                     print("Turno cancelado con exito")
+#                 elif lunes4 == paciente:
+#                     lunes4 = ""
+#                     print("Turno cancelado con exito")
+#                 else:
+#                     print("El paciente no tiene turno asignado")
+#             else:
+#                 if martes1 == paciente:
+#                     martes1 = ""
+#                     print("Turno cancelado con exito")
+#                 elif martes2 == paciente:
+#                     martes2 = ""
+#                     print("Turno cancelado con exito")
+#                 elif martes3 == paciente:
+#                     martes3 = ""
+#                     print("Turno cancelado con exito")
+#                 else:
+#                     print("El paciente no tiene turno asignado")
+#         # OPCION 3
+#         elif opcion == 3:
+#             while True:
+#                 print("1- LUNES")
+#                 print("2- MARTES")
+#                 dia = input("Selecione un dia:")
+
+#                 if dia == "":
+#                     print("Debe ingresar una opcion para continuar")
+#                     print("")
+#                 elif not dia.isdigit():
+#                     print("Ingrese un valor numerico")
+#                     print("")
+#                 else:
+#                     dia = int(dia)
+
+#                     if dia < 1 or dia > 2:
+#                         print("Error opcion fuera de rango")
+#                     else:
+#                         break
+#             if dia == 1:
+#                 print("Agenda dia lunes")
+#                 if lunes1 == "":
+#                     print("Turno 1 libre")
+#                 else:
+#                     print(f"Turno 1: {lunes1}")
+#                 if lunes2 == "":
+#                     print("Turno 2 libre")
+#                 else:
+#                     print(f"Turno 2: {lunes2}")
+#                 if lunes3 == "":
+#                     print("Turno 3 libre")
+#                 else:
+#                     print(f"Turno 3: {lunes3}")
+#                 if lunes4 == "":
+#                     print("Turno 4 libre")
+#                 else:
+#                     print(f"Turno 4: {lunes4}")
+#             else:
+#                 print("Agenda dia martes")
+#                 if martes1 == "":
+#                     print("Turno 1 libre")
+#                 else:
+#                     print(f"Turno 1: {martes1}")
+#                 if martes2 == "":
+#                     print("Turno 2 libre")
+#                 else:
+#                     print(f"Turno 2: {martes2}")
+#                 if martes3 == "":
+#                     print("Turno 3 libre")
+#                 else:
+#                     print(f"Turno 3: {martes3}")
+
+#         # OPCION 4
+#         elif opcion == 4:
+#             ocupados_lunes = 0
+#             ocupados_martes = 0
+
+#             # Contador
+#             if lunes1 != "":
+#                 ocupados_lunes = ocupados_lunes + 1
+
+#             if lunes2 != "":
+#                 ocupados_lunes = ocupados_lunes + 1
+
+#             if lunes3 != "":
+#                 ocupados_lunes = ocupados_lunes + 1
+
+#             if lunes4 != "":
+#                 ocupados_lunes = ocupados_lunes + 1
+
+#             # Contador
+#             if martes1 != "":
+#                 ocupados_martes = ocupados_martes + 1
+
+#             if martes2 != "":
+#                 ocupados_martes = ocupados_martes + 1
+
+#             if martes3 != "":
+#                 ocupados_martes = ocupados_martes + 1
+
+#             # Calculos de dias
+#             disponibles_lunes = 4 - ocupados_lunes
+#             disponibles_martes = 3 - ocupados_martes
+
+#             # Mostrar resumen
+#             print("----- RESUMEN GENERAL -----")
+#             print(f"Lunes: {ocupados_lunes} ocupados - {disponibles_lunes} disponibles")
+#             print(
+#                 f"Martes: {ocupados_martes} ocupados - {disponibles_martes} disponibles"
+#             )
+
+#             # Moastrar resumen
+#             if ocupados_lunes > ocupados_martes:
+#                 print("El lunes tiene más turnos ocupados")
+#             elif ocupados_martes > ocupados_lunes:
+#                 print("El martes tiene más turnos ocupados")
+#             else:
+#                 print("Hay empate en la cantidad de turnos ocupados")
+#         # OPCION 5
+#         elif opcion == 5:
+#             break
